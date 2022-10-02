4cab8522982463b896f41f80bd4250b8b15597a1
Kamil <kamil.opsm@wp.pl>
2022-10-02 19:47:51 +0200

    removed and added new section main, working on media on this section

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 const navBtn = document.querySelector(".nav__bars-btn");
 const slider = document.querySelector(".nav__slider");
+const mainItem = document.querySelectorAll(".main__item");
+const mainTextBox = document.querySelector(".main__text-box");
 
 const handleSliderNav = () => {
 	slider.classList.toggle("transform-class");
